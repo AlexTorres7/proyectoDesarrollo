7d52622a352c6f95e9a321b049a4c29c6d4441b8
bgarciag <bran.and.gg@gmail.com>
2022-12-20 17:42:49 -0600

    Se agrego TestNumber.vue

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,18 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <!--<HelloWorld msg="Welcome to Your Vue.js App"/>-->
+  <TestNumber msg="Hola"/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+//import HelloWorld from './components/HelloWorld.vue'
+import TestNumber from './components/TestNumber.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    //HelloWorld
+    TestNumber
   }
 }
 </script>
