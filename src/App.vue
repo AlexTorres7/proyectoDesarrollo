88221b02fe291194ee6bdecc3a754663c517d328
Kevin Alejandro Torres Perez <katorres@frd.org.mx>
2022-12-22 11:43:06 -0600

    Se modifico el front del home

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,54 @@
 <template>
   
-  <nav>
-
-    <router-link to="/CrearColaborador">Crear Colaborador</router-link> |
-    <router-link to="/ConsultarColaborador">ConsultarColaborador</router-link> |
-    <router-link to="/ActualizarColaborador">ActualizarColaborador</router-link> |
-    <router-link to="/EliminarColaborador">EliminarColaborador</router-link>
+  <nav id="head">
+    <button><img alt="FRD" src="./assets/dondé-logo.png" id="img"></button>
+    
+    <router-link to="/CrearColaborador">Crear Colaborador</router-link> 
+    <router-link to="/ConsultarColaborador">Consultar Colaborador</router-link> 
+    <router-link to="/ActualizarColaborador">Actualizar Colaborador</router-link> 
+    <router-link to="/EliminarColaborador">Eliminar Colaborador</router-link>
 
   </nav>
   <router-view/>
 </template>
 
+<style>
+
+@import url('https://fonts.googleapis.com/css2?family=PT+Serif:ital,wght@0,400;0,700;1,400;1,700&family=Poppins:ital,wght@0,300;0,500;1,300;1,500&display=swap');
+nav{
+  /*font-family: 'Poppins', sans-serif;*/
+  font-family: 'PT Serif', serif;
+background-color: green;
+display: flex;
+width: 100%;
+height: 60px;
+}
+
+nav button{
+width: 60px;
+height: 60px;
+background-color: green;
+border: 0;
+margin-right: auto;
+}
+
+nav img{
+width: 50px;
+height: 50px;
+}
+
+nav a{
+    display: flex;
+    color: white;
+    padding: 12px;
+    text-decoration: none;
+    font-size: 15px;
+}
+
+nav a:hover{
+  color: white;
+  font-weight: bolder;
+  text-align: center;
+}
+
+</style>
