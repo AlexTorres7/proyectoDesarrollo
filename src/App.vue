4060b05899acc08630e30d30aeeb14f39d549b13
gabriel poot <gabriel_mau598@hotmail.com>
2022-12-22 11:37:48 -0600

    se modifico frond consultar colaboradores

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,12 @@
 <template>
   
   <nav>
-
+ 
     <router-link to="/CrearColaborador">Crear Colaborador</router-link> |
     <router-link to="/ConsultarColaborador">ConsultarColaborador</router-link> |
     <router-link to="/ActualizarColaborador">ActualizarColaborador</router-link> |
     <router-link to="/EliminarColaborador">EliminarColaborador</router-link>
+    
 
   </nav>
   <router-view/>
